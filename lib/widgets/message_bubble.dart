7668327e362dcf3a992291c888993e946ecf6ec6
Stormer <Samuelayodeji.ask@gmail.com>
2024-01-03 00:25:40 +0700

    Second commit

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -11,7 +11,7 @@ class MessageBubble extends StatelessWidget {
     required this.isMe,
   }) : isFirstInSequence = true;
 
-  // Create a amessage bubble that continues the sequence.
+  
   const MessageBubble.next({
     super.key,
     required this.message,
@@ -20,23 +20,17 @@ class MessageBubble extends StatelessWidget {
         userImage = null,
         username = null;
 
-  // Whether or not this message bubble is the first in a sequence of messages
-  // from the same user.
-  // Modifies the message bubble slightly for these different cases - only
-  // shows user image for the first message from the same user, and changes
-  // the shape of the bubble for messages thereafter.
+  
   final bool isFirstInSequence;
 
-  // Image of the user to be displayed next to the bubble.
-  // Not required if the message is not the first in a sequence.
+  
   final String? userImage;
 
-  // Username of the user.
-  // Not required if the message is not the first in a sequence.
+  
   final String? username;
   final String message;
 
-  // Controls how the MessageBubble will be aligned.
+  
   final bool isMe;
 
   @override
@@ -59,11 +53,10 @@ class MessageBubble extends StatelessWidget {
             ),
           ),
         Container(
-          // Add some margin to the edges of the messages, to allow space for the
-          // user's image.
+          
           margin: const EdgeInsets.symmetric(horizontal: 46),
           child: Row(
-            // The side of the chat screen the message should show at.
+            
             mainAxisAlignment:
                 isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
             children: [
@@ -89,16 +82,13 @@ class MessageBubble extends StatelessWidget {
                       ),
                     ),
 
-                  // The "speech" box surrounding the message.
+                  
                   Container(
                     decoration: BoxDecoration(
                       color: isMe
                           ? Colors.grey[300]
                           : theme.colorScheme.secondary.withAlpha(200),
-                      // Only show the message bubble's "speaking edge" if first in
-                      // the chain.
-                      // Whether the "speaking edge" is on the left or right depends
-                      // on whether or not the message bubble is the current user.
+                      
                       borderRadius: BorderRadius.only(
                         topLeft: !isMe && isFirstInSequence
                             ? Radius.zero
@@ -110,9 +100,7 @@ class MessageBubble extends StatelessWidget {
                         bottomRight: const Radius.circular(12),
                       ),
                     ),
-                    // Set some reasonable constraints on the width of the
-                    // message bubble so it can adjust to the amount of text
-                    // it should show.
+                    
                     constraints: const BoxConstraints(maxWidth: 200),
                     padding: const EdgeInsets.symmetric(
                       vertical: 10,
@@ -126,8 +114,7 @@ class MessageBubble extends StatelessWidget {
                     child: Text(
                       message,
                       style: TextStyle(
-                        // Add a little line spacing to make the text look nicer
-                        // when multilined.
+                        
                         height: 1.3,
                         color: isMe
                             ? Colors.black87
